063b7a8e39843161c0c11030d29cd40717f36c9e
RCElucidator7 <G00332826@gmit.ie>
2019-03-13 17:39:43 +0000

    Added Shooting mechanic with mouse

---
+++ Gesture-Based-UI-Project/Assets/Scripts/Shooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 100f;
+
+	public Camera fps;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Shoot();
+		}
+	}
+
+	void Shoot(){
+		RaycastHit hit;
+		if(Physics.Raycast(fps.transform.position, fps.transform.forward, out hit, range)){
+			Debug.Log(hit.transform.name);
+
+			Target target = hit.transform.GetComponent<Target>();
+			if(target != null){
+				target.TakeDamage(damage);
+			}
+		}
+	}
+}
