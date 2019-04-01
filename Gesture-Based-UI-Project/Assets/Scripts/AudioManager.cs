1f44b08de29243c5005a74cb2cae873f4a3f41bd
RCElucidator7 <G00332826@gmit.ie>
2019-04-01 17:11:24 +0100

    Added Audio and Splash screen

---
+++ Gesture-Based-UI-Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	static AudioManager instance = null;
+
+    //Keep the audio playing when navigating to a different scene
+    private void Awake(){
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
