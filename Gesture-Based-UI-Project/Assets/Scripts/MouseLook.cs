d5f38925b0388ea74669f60b955eba37d87cc72c
RCElucidator7 <G00332826@gmit.ie>
2019-04-02 21:02:01 +0100

    Gun following camera

--- Gesture-Based-UI-Project/Assets/Scripts/MouseLook.cs
+++ Gesture-Based-UI-Project/Assets/Scripts/MouseLook.cs
@@ -18,50 +18,70 @@ using System.Collections;
 public class MouseLook : MonoBehaviour
 {
 
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-    public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
-
-    public float minimumX = -360F;
-    public float maximumX = 360F;
-
-    public float minimumY = -60F;
-    public float maximumY = 60F;
-
-    float rotationY = 0F;
-
-    void Update()
-    {
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        }
-    }
-
-    void Start()
-    {
-        //if(!networkView.isMine)
-        //enabled = false;
-
-        // Make the rigid body not change rotation
-        //if (rigidbody)
-        //rigidbody.freezeRotation = true;
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, 	MouseY = 2 };
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	 
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	 
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	 
+	float rotationX = 0F;
+	float rotationY = 0F;
+	 
+	Quaternion originalRotation;
+	 
+	void Update()
+	{
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			// Read the mouse input axis
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			 
+			rotationX = ClampAngle(rotationX, minimumX, maximumX);
+			rotationY = ClampAngle(rotationY, minimumY, maximumY);
+			 
+			Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+			Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+			 
+			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationX = ClampAngle(rotationX, minimumX, maximumX);
+			 
+			Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+			transform.localRotation = originalRotation * xQuaternion;
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = ClampAngle(rotationY, minimumY, maximumY);
+			 
+			Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+			transform.localRotation = originalRotation * yQuaternion;
+		}
+	}
+	 
+	void Start()
+	{
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
+			GetComponent<Rigidbody>().freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+	 
+	public static float ClampAngle(float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp(angle, min, max);
     }
 }
