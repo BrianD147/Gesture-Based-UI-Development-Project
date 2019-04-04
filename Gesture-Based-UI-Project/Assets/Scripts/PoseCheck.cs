cdd1864787cfb145ad686aa9b722a376afb962f3
RCElucidator7 <G00332826@gmit.ie>
2019-04-04 22:05:01 +0100

    Fixed Canvas sizing issues - added to presentation

--- Gesture-Based-UI-Project/Assets/Scripts/PoseCheck.cs
+++ Gesture-Based-UI-Project/Assets/Scripts/PoseCheck.cs
@@ -5,12 +5,11 @@ using LockingPolicy = Thalmic.Myo.LockingPolicy;
 using Pose = Thalmic.Myo.Pose;
 using UnlockType = Thalmic.Myo.UnlockType;
 using VibrationType = Thalmic.Myo.VibrationType;
-using UnityEngine.SceneManagement;
 
 public class PoseCheck : MonoBehaviour {
 
 	public bool isPoseCheckEnabled = true;
-    private GameObject myoGameObject;
+    public GameObject myoGameObject;
     public float checkNewMyoPoseRate = 1f;
     private float nextMyoPoseCheck = 0f;
 
@@ -24,21 +23,9 @@ public class PoseCheck : MonoBehaviour {
 
     public delegate void PoseAction();
 
-	public GameObject PauseMenuUI;
-	private bool isGamePaused = false;
-
 	// Use this for initialization
 	void Start () {
-		myoGameObject = GameObject.FindGameObjectWithTag("myo");
 		myo = myoGameObject.GetComponent<ThalmicMyo> ();
-
-		PlayerPrefs.SetString("lastLoadedScene", SceneManager.GetActiveScene().name);
-		Debug.Log(PlayerPrefs.GetString("lastSceneLoaded"));
-
-		if (PlayerPrefs.GetInt("isPauseLoaded") == 1)
-		{
-			Pause();
-		}
 	}
 	
 	// Update is called once per frame
@@ -65,22 +52,16 @@ public class PoseCheck : MonoBehaviour {
 		switch(myo.pose)
 		{
 			case Pose.FingersSpread:
-				if(PlayerPrefs.GetInt("isPauseLoaded") == 1)
-					Options();
+				
 				break;
 			case Pose.Fist:
-				if (PlayerPrefs.GetInt("isPauseLoaded") == 0)
-					Shoot();
-				else
-					Quit();
+				Shoot();
 				break;
 			case Pose.WaveIn:
-				if(PlayerPrefs.GetInt("isPauseLoaded") == 1)
-					Resume();
+				
 				break;
 			case Pose.WaveOut:
-				if(PlayerPrefs.GetInt("isPauseLoaded") == 0)
-					Pause();
+				
 				break;
 			case Pose.DoubleTap:
 
@@ -98,38 +79,10 @@ public class PoseCheck : MonoBehaviour {
 		RaycastHit hit;
 		//If the raycast hits an object, the object will lose health until it is destroyed
 		if(Physics.Raycast(fps.transform.position, fps.transform.forward, out hit, range)){
-			//Debug.Log(hit.transform.name);
-
 			Target target = hit.transform.GetComponent<Target>();
 			if(target != null){
 				target.TakeDamage(damage);
 			}
 		}
-	}
-
-	public void Pause () {
-        Debug.Log("Pausing Game");
-		PlayerPrefs.SetInt("isPauseLoaded", 1);
-		Time.timeScale = 0f;
-		PauseMenuUI.SetActive(true);
-    }
-
-	public void Resume () {
-        Debug.Log("Pausing Game");
-		PlayerPrefs.SetInt("isPauseLoaded", 0);
-		PauseMenuUI.SetActive(false);
-		Time.timeScale = 1f;
-    }
-
-	public void Options () {
-        Debug.Log("Controls/Options Selected");
-        SceneManager.LoadScene("Options");
-    }
-
-	public void Quit() {
-		Debug.Log("Quiting Current Game");
-		PlayerPrefs.SetInt("isPauseLoaded", 0);
-		SceneManager.LoadScene("MainMenu");
-		Time.timeScale = 1f;
 	}
 }
