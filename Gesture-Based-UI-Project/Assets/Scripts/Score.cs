4b7e0665683e3aa865f0ee3f0eb314cdae368311
BrianD147 <briandoyle147@gmail.com>
2019-04-09 15:24:08 +0100

    Added scoring system and level complete navigation

---
+++ Gesture-Based-UI-Project/Assets/Scripts/Score.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	private Scene scene;
+	private string sceneName;
+
+
+	// Use this for initialization
+	void Start () {
+		scene = SceneManager.GetActiveScene();
+		sceneName = scene.name;
+		switch(sceneName) {
+			case "Level-1":
+				PlayerPrefs.SetInt("Score", 3);
+				break;
+			case "Level-2":
+				PlayerPrefs.SetInt("Score", 4);
+				break;
+			case "Level-3":
+				PlayerPrefs.SetInt("Score", 5);
+				break;
+			case "Level-example":
+				PlayerPrefs.SetInt("Score", 5);
+				break;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = PlayerPrefs.GetInt("Score").ToString();
+		if(PlayerPrefs.GetInt("Score") == 0){
+			LevelComplete();
+		}
+	}
+
+	public void LevelComplete(){
+		SceneManager.LoadScene(scene.buildIndex + 1);
+	}
+}
--- Gesture-Based-UI-Project/Assets/Scripts/Target.cs
+++ Gesture-Based-UI-Project/Assets/Scripts/Target.cs
@@ -6,12 +6,16 @@ public class Target : MonoBehaviour {
 
 	//Health of the target object
 	public float health = 50f;
+	private int score;
 
 	public void TakeDamage(float amount){
 		//Take health away from  the object, if it hits zero call the Die() method
 		health -= amount;
 		if(health <= 0){
 			Die();
+			score = PlayerPrefs.GetInt("Score");
+			score--;
+			PlayerPrefs.SetInt("Score", score);
 		}
 	}
 
